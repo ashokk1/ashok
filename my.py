a3a17e5d6b3b1b93487649dff9dccd852c87b39c
Ashokk1 <ashok.kothapalle404@gmail.com>
2019-02-18 12:10:10 +0530

    Initial commit

---
+++ my.py
@@ -0,0 +1,16 @@
+#1Reverse string 'MANAGEMENT'?
+st='MANAGEMENT'
+st1=st[::-1]
+print(st1)
+
+#2)whatis tuple and list?
+#tuple
+tup=(5,10,20,30,40)
+#it is immutable i.e once you create tuple objects we cant alter it
+#list
+lst=[1,3,5,6]
+#list is mutable i.e we can increase or decrease lenth of list
+#3)st='MACHINE' OUT PUT HINE
+st='MACHINE'
+st1=st[3:]
+print(st1)
